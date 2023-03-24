e2418f7fa9a8552689c261f47616b2290fff1a99
yiitgven7x <52351364+7xyiit@users.noreply.github.com>
2023-03-24 13:16:19 +0300

    Add files via upload

---
+++ Valorant Instalock.py
@@ -0,0 +1,389 @@
+import tkinter as tk
+import keyboard
+import pyautogui as pg
+
+ekran = tk.Tk()
+ekran.geometry('300x500')
+ekran.attributes('-topmost',True)
+ekran.title('Valorant Instalock')
+ekran.resizable(False, False)
+
+def astra():
+    while True:
+        pg.moveTo(540,925)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def breach():
+    while True:
+        pg.moveTo(623,922)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def brimstone():
+    while True:
+        pg.moveTo(708,924)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def chamber():
+    while True:
+        pg.moveTo(791,924)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def cypher():
+    while True:
+        pg.moveTo(878,926)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def fade():
+    while True:
+        pg.moveTo(962,923)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def gekko():
+    while True:
+        pg.moveTo(1042,926)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def harbor():
+    while True:
+        pg.moveTo(1130,917)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def jett():
+    while True:
+        pg.moveTo(1215,922)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def kayo():
+    while True:
+        pg.moveTo(1301,918)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def killjoy():
+    while True:
+        pg.moveTo(1380,926)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def neon():
+    while True:
+        pg.moveTo(538,1006)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def omen():
+    while True:
+        pg.moveTo(624,1005)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def phoenix():
+    while True:
+        pg.moveTo(708,1003)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def raze():
+    while True:
+        pg.moveTo(795,997)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def reyna():
+    while True:
+        pg.moveTo(873,1001)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def sage():
+    while True:
+        pg.moveTo(967,1004)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def skye():
+    while True:
+        pg.moveTo(1047,999)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def sova():
+    while True:
+        pg.moveTo(1127,999)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def viper():
+    while True:
+        pg.moveTo(1212,1001)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break
+
+def yoru():
+    while True:
+        pg.moveTo(1294,1002)
+        pg.doubleClick()
+        pg.PAUSE=0.035
+        pg.moveTo(954,811)
+        pg.doubleClick()
+
+        try:
+            if keyboard.is_pressed('p'):
+                print("Program kapatıldı.")
+                break
+        except:
+            break 
+
+astra_instalock = tk.Button(text='Astra', command=astra, bg='purple', fg='white', font=('Arial', 12))
+astra_instalock.grid(padx=15,pady=15, row=1, column=0)
+
+breach_instalock = tk.Button(text='Breach', command=breach, bg='orange', fg='white', font=('Arial', 12))
+breach_instalock.grid(padx=15,pady=15, row=2, column=0)
+
+brimstone_instalock = tk.Button(text='Brimstone', command=brimstone, bg='brown', fg='white', font=('Arial', 12))
+brimstone_instalock.grid(padx=15,pady=15, row=3, column=0)
+
+chamber_instalock = tk.Button(text='Chamber', command=chamber, bg='gold', fg='Black', font=('Arial', 12))
+chamber_instalock.grid(padx=15,pady=15, row=4, column=0)
+
+cypher_instalock = tk.Button(text='Cypher', command=cypher, bg='white', fg='Black', font=('Arial', 12))
+cypher_instalock.grid(padx=15,pady=15, row=5, column=0)
+
+fade_instalock = tk.Button(text='Fade', command=fade, bg='Black', fg='White', font=('Arial', 12))
+fade_instalock.grid(padx=15,pady=15, row=6, column=0)
+
+gekko_instalock = tk.Button(text='Gekko', command=gekko, bg='green', fg='White', font=('Arial', 12))
+gekko_instalock.grid(padx=15,pady=15, row=7, column=0)
+
+harbor_instalock = tk.Button(text='Harbor', command=harbor, bg='darkblue', fg='White', font=('Arial', 12))
+harbor_instalock.grid(padx=15,pady=15, row=8, column=0)
+
+jett_instalock = tk.Button(text='Jett', command=jett, bg='turquoise', fg='White', font=('Arial', 12))
+jett_instalock.grid(padx=15,pady=15, row=1, column=1)
+
+kayo_instalock = tk.Button(text='Kayo', command=kayo, bg='darkturquoise', fg='White', font=('Arial', 12))
+kayo_instalock.grid(padx=15,pady=15, row=2, column=1)
+
+killjoy_instalock = tk.Button(text='Killjoy', command=killjoy, bg='pink', fg='White', font=('Arial', 12))
+killjoy_instalock.grid(padx=15,pady=15, row=3, column=1)
+
+neon_instalock = tk.Button(text='Neon', command=neon, bg='yellow', fg='turquoise', font=('Arial', 12))
+neon_instalock.grid(padx=15,pady=15, row=4, column=1)
+
+omen_instalock = tk.Button(text='Omen', command=omen, bg='darkslateblue', fg='white', font=('Arial', 12))
+omen_instalock.grid(padx=15,pady=15, row=5, column=1)
+
+phoenix_instalock = tk.Button(text='Phoenix', command=phoenix, bg='darkorange2', fg='white', font=('Arial', 12))
+phoenix_instalock.grid(padx=15,pady=15, row=6, column=1)
+
+raze_instalock = tk.Button(text='Raze', command=raze, bg='darkorange4', fg='white', font=('Arial', 12))
+raze_instalock.grid(padx=15,pady=15, row=7, column=1)
+
+reyna_instalock = tk.Button(text='Reyna', command=reyna, bg='darkviolet', fg='white', font=('Arial', 12))
+reyna_instalock.grid(padx=15,pady=15, row=8, column=1)
+
+sage_instalock = tk.Button(text='Sage', command=sage, bg='darkslategray4', fg='white', font=('Arial', 12))
+sage_instalock.grid(padx=15,pady=15, row=1, column=2)
+
+skye_instalock = tk.Button(text='Skye', command=skye, bg='darkseagreen3', fg='white', font=('Arial', 12))
+skye_instalock.grid(padx=15,pady=15, row=2, column=2)
+
+sova_instalock = tk.Button(text='Sova', command=sova, bg='darkturquoise', fg='white', font=('Arial', 12))
+sova_instalock.grid(padx=15,pady=15, row=3, column=2)
+
+viper_instalock = tk.Button(text='Viper', command=viper, bg='forestgreen', fg='white', font=('Arial', 12))
+viper_instalock.grid(padx=15,pady=15, row=4, column=2)
+
+yoru_instalock = tk.Button(text='Yoru', command=yoru, bg='dodgerblue4', fg='white', font=('Arial', 12))
+yoru_instalock.grid(padx=15,pady=15, row=5, column=2)
+
+ekran.mainloop()
